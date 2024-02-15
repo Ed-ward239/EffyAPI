eb4a62aad34e260e3510dd49041027f7558e69a7
Edward Lee <edward@effyjewelry.com>
2024-02-15 11:02:23 -0500

    index->app

--- App.test.js
+++ App.test.js
@@ -1,6 +1,6 @@
 // index.test.js for backend API
 const request = require('supertest');
-const app = require('./index');
+const app = require('./App');
 
 test('CCL_GET', async () => {
   const response = await request(app).get('/ccl_get');
