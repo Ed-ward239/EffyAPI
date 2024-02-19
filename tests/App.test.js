d8cefeaafe2c9bc1e1e39f500c7f3ad755ac2ab5
Ed-ward239 <edward9ed@gmail.com>
2024-02-19 12:09:57 -0500

    testing azure

--- tests/App.test.js
+++ tests/App.test.js
@@ -8,49 +8,6 @@ require('dotenv').config();
 test("Get CCL", async () => {
   const response = await request(app).get("/ccl_get");
   expect(response.status).toBe(200);
-  expect(response.body).toEqual([
-    {
-      ship_name: "APEX",
-      voyage_num: "AP20271122005",
-      date: "2027-11-22T00:00:00.000Z",
-      status_paid: "Pending",
-      editor: "Edward",
-      rev_ss: 17888,
-      rev_cc: 23288,
-      eu_vat: null,
-      carnival_share: -15688.32,
-      office_supp: null,
-      discounts: 188.12,
-      exec_folio: -188.12,
-      ss_fee: -228,
-      cc_fee: -588.0599,
-      meal_charge: -488,
-      parole_fee: null,
-      cash_adv: null,
-      cash_paid: null,
-      effy_share: 23988,
-    },
-    {
-      ship_name: "APEX",
-      voyage_num: "AP20480925005",
-      date: "2048-09-25T00:00:00.000Z",
-      status_paid: "Pending",
-      editor: "Edward",
-      rev_ss: 44365,
-      rev_cc: null,
-      eu_vat: null,
-      carnival_share: -16858.7,
-      office_supp: null,
-      discounts: null,
-      exec_folio: -189.85,
-      ss_fee: -597.23,
-      cc_fee: null,
-      meal_charge: -440,
-      parole_fee: -46,
-      cash_adv: null,
-      cash_paid: null,
-      effy_share: 26233.22,
-    },
-  ]);
+  expect(response.body).toEqual([{"carnival_share": -15688.32, "cash_adv": null, "cash_paid": null, "cc_fee": -588.0599, "date": "2027-11-22T00:00:00.000Z", "discounts": 188.12, "editor": "Edward", "effy_share": 23988, "eu_vat": null, "exec_folio": -188.12, "meal_charge": -488, "office_supp": null, "parole_fee": null, "rev_cc": 23288, "rev_ss": 17888, "ship_name": "APEX", "ss_fee": -228, "status_paid": "Pending", "voyage_num": "AP20271122005"}, {"carnival_share": -16858.7, "cash_adv": null, "cash_paid": null, "cc_fee": null, "date": "2048-09-25T00:00:00.000Z", "discounts": null, "editor": "Edward", "effy_share": 26233.22, "eu_vat": null, "exec_folio": -189.85, "meal_charge": -440, "office_supp": null, "parole_fee": -46, "rev_cc": null, "rev_ss": 44365, "ship_name": "APEX", "ss_fee": -597.23, "status_paid": "Pending", "voyage_num": "AP20480925005"}]);
 }, 20000);
 
